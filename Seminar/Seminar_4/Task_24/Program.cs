1f3f08daf6a5bdb3bd0d71ef49e30243ee0194aa
ElmiraFaizova <patent_k@mail.ru>
2022-08-16 12:22:06 +0400

    C#

---
+++ Seminar/Seminar_4/Task_24/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//  7 -> 28
+//  4 -> 10
+//  8 -> 36
+
+//int Sum(int a)
+//{
+//   int result = 0;
+//   for (int number = 0; number <= a; number++) 
+//   {
+//      result += number;  //result=result+number;
+//  }
+//   return result;
+//}
+//Console.WriteLine(Sum(5));  //1+2+3+4+5=15
+//Console.WriteLine(Sum(10));
+//Console.WriteLine(Sum(0));
+//Console.WriteLine(Sum(-20));
+
+// Второй вариант(улучшенный)
+
+int Sum(int a)
+{
+    int result = 0;
+    for (int number = 0; number <= a; number++)
+    {
+        result += number;
+    }
+    return result;
+}
+
+void PrintSum(int a)
+{
+    Console.WriteLine($"Сумма чисел от 1 до {a} равна {Sum(a)}");
+}
+
+PrintSum(5);  //1+2+3+4+5=15
+PrintSum(10);
+PrintSum(0);
+PrintSum(-20);
---
+++ Seminar/Seminar_4/Task_26/Program.cs
@@ -0,0 +1,65 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//   456 -> 3
+//    78 -> 2
+// 89126 -> 5
+//
+//int LenNumber(int number)
+//{
+//    int mylen = 0;
+//   while (number != 0) ;
+//   {
+//
+//      mylen++;
+//      number = number / 10;
+//  }
+//  return mylen;
+//}
+//void PrintRes(int res)
+//{
+//    Console.WriteLine(LenNumber(res));
+//}
+//PrintRes(10000);
+//
+// Второй вариант
+
+int Digits(int number)
+{
+    int result = Math.Abs(number).ToString().Length;
+    return result;
+}
+
+int Digitsv2(int number)
+{
+    int n = number;
+    int result = 1;
+
+    while (n >= 10)
+    {
+        n /= 10;
+        result++;
+    }
+
+    return result;
+}
+
+string Decline(int number)
+{
+    if ((number >= 11 && number <= 19) || number % 10 >= 5) return "цифр";
+    else if (number % 10 >= 2 && number % 10 <= 4) return "цифры";
+    else return "цифру";
+}
+
+void PrintDigits(int number)
+{
+    Console.WriteLine($"Число {number} содержит {Digits(number)} {Decline(Digits(number))}.");
+}
+
+void PrintDigitsv2(int number)
+{
+    Console.WriteLine($"Число {number} содержит {Digitsv2(number)} {Decline(Digitsv2(number))}.");
+}
+
+PrintDigits(7);
+PrintDigitsv2(10);
+PrintDigits(456);
+PrintDigitsv2(89126);
