93449e7adcc3e33eadc1c1f850bb2f4412f4cfd3
ElmiraFaizova <patent_k@mail.ru>
2022-08-24 16:51:58 +0400

    Добавила исправленный вариант

--- Homework/DZ_6/Task_43/Program.cs
+++ Homework/DZ_6/Task_43/Program.cs
@@ -3,6 +3,46 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+
+// Первоначальный вариант
+//Console.WriteLine("Введите значение b1: ");
+//double b1 = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Введите значение k1: ");
+//double k1 = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Введите значение b2: ");
+//double b2 = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Введите значение k2: ");
+//double k2 = Convert.ToDouble(Console.ReadLine());
+
+//double x = (b2 - b1) / (k1 - k2); // Приравниваем правые части уравнений друг к другу чтобы получить  x: k1 * x + b1 = k2 * x + b2;
+//double y = k1 * x + b1;           // Подставляем х в любое из уравнений, чтобы получить y:
+
+//Console.WriteLine($"Точкой пересечения прямых является ({x}, {y}).");
+
+// Исправленный вариант
+void GetCrossPoint(double b1, double k1, double b2, double k2)
+{
+    if ((k1 - k2) == 0)
+    {
+        if (b1 == b2)
+        {
+            Console.WriteLine("Прямые совпадают.");
+        }
+        else
+        {
+            Console.WriteLine("Решения нет");
+        }
+    }
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Точкой пересечения двух прямых является ({x}, {y}).");
+    }
+}
 Console.WriteLine("Введите значение b1: ");
 double b1 = Convert.ToDouble(Console.ReadLine());
 
@@ -14,8 +54,4 @@ double b2 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Введите значение k2: ");
 double k2 = Convert.ToDouble(Console.ReadLine());
-
-double x = (b2 - b1) / (k1 - k2); // Приравниваем правые части уравнений друг к другу чтобы получить  x: k1 * x + b1 = k2 * x + b2;
-double y = k1 * x + b1;           // Подставляем х в любое из уравнений, чтобы получить y:
-
-Console.WriteLine($"Точкой пересечения прямых является ({x}, {y}).");
+GetCrossPoint(b1, k1, b2, k2);
