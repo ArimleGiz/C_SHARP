499b9812730f505c89ad05275c9f446aaa311257
ElmiraFaizova <patent_k@mail.ru>
2022-09-01 12:31:24 +0400

    C#

---
+++ Seminar/Seminar_8/Task_57/Program.cs
@@ -0,0 +1,94 @@
+﻿/*
+Составить частотный словарь элементов двумерного массива. 
+Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+ Набор данных                Частотный массив
+  1, 2, 3                1 встречается 3 раза
+  4, 6, 1                2 встречается 2 раз
+  2, 1, 6                3 встречается 1 раз
+                         4 встречается 1 раз
+                         6 встречается 2 раза
+*/
+int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int[,] matrix = new int[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            matrix[row, column] = new Random().Next(min, max+1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int column = 0; column < matrix.GetLength(1); column++)
+        {
+            Console.Write($"{matrix[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] FlattenMatrix(int[,] matrix)
+{
+    int[] array = new int[matrix.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = matrix[i / matrix.GetLength(1), i % matrix.GetLength(1)];
+    }
+    return array;
+}
+
+int[] SortArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[i])
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+void PrintFrequencies(int[] arr)
+{
+    int count = 1;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] == arr[i - 1])
+        {
+            count++;
+        }
+        else
+        {
+            Console.WriteLine($"{arr[i - 1]} встречается {count} раз");
+            count = 1;
+        }
+
+        if (i == arr.Length - 1)
+        {
+            Console.WriteLine($"{arr[i]} встречается {count} раз");
+        }
+    }
+}
+
+int[,] matrix = CreateMatrix(3, 3, 1, 6);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+int[] array = FlattenMatrix(matrix);
+
+int[] sorted = SortArray(array);
+
+Console.WriteLine();
+PrintFrequencies(sorted);
---
+++ Seminar/Seminar_8/Task_59/Program.cs
@@ -0,0 +1,95 @@
+﻿/*
+Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, 
+на пересечении которых расположен наименьший элемент массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Наименьший элемент - 1, на выходе получим 
+следующий массив:
+9 4 2
+2 2 6
+3 4 7
+*/
+
+int[,] GetMatrix(int raw, int col, int min, int max)
+{
+    int[,] matrix = new int[raw, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}  ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int[] GetNumsMinRawColumn(int[,] array)     // поиск минимального элемента массива и номеров строки и столбца 
+{
+    int[] numbers = new int[] { 0, 0 };
+    int minNumber = array[0, 0];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < minNumber)
+            {
+                minNumber = array[i, j];
+                numbers[0] = i;
+                numbers[1] = j;
+            }
+        }
+    }
+    return numbers;
+}
+
+int[,] RemoteRawColumn(int[,] matrix, int[] array)
+{
+    int[,] matrix2 = new int[matrix.GetLength(0) - 1, matrix.GetLength(1)]; // удаляем строку из исходного массива
+    for (int i = 0; i < matrix2.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            if (i < array[0]) matrix2[i, j] = matrix[i, j];
+            else matrix2[i, j] = matrix[i + 1, j];
+        }
+    }
+    int[,] resultMatrix = new int[matrix2.GetLength(0), matrix2.GetLength(1) - 1]; // удаляем столбец из массива с удаленной строкой
+    {
+        for (int i = 0; i < resultMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < resultMatrix.GetLength(1); j++)
+            {
+                if (j < array[1]) resultMatrix[i, j] = matrix2[i, j];
+                else resultMatrix[i, j] = matrix2[i, j + 1];
+            }
+        }
+    }
+    return resultMatrix;
+}
+
+int raw = 4;
+int col = 4;
+int min = 1;
+int max = 10;
+
+int[,] matrix = GetMatrix(raw, col, min, max);
+PrintArray(matrix);
+int[] minPosition = GetNumsMinRawColumn(matrix);
+int[,] matrixWithoutRawCol = RemoteRawColumn(matrix, minPosition);
+PrintArray(matrixWithoutRawCol);
